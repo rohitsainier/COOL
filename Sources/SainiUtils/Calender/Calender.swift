67616c99682d0c2a196daf6e4f5bbefd2c084a0e
rohitsainier <rohitsainier@gmail.com>
2021-11-11 00:16:42 +0530

    predicate issue fixed in get events from calender

--- Sources/SainiUtils/Calender/Calender.swift
+++ Sources/SainiUtils/Calender/Calender.swift
@@ -40,49 +40,57 @@ public func getEventsFromCalendar(completion: @escaping (_ events: [EKEvent]) ->
         if (granted) && (error == nil) {
             let startDate = Date()
             let endDate = Date().addingTimeInterval(60 * 60 * 24 * 365)
-            let events = eventStore.events(matching: NSPredicate(format: "startDate >= %@ AND endDate <= %@", startDate as CVarArg, endDate as CVarArg))
+            let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+            let events = eventStore.events(matching: predicate)
             completion(events)
         }
     }
 }
 
-//MARK: - Delete event from calendar
-public func deleteEventFromCalendar(event: EKEvent, completion: @escaping (_ success: Bool) -> Void) {
+//MARK: - Delete event from calendar by title
+func deleteEventFromCalendar(title: String, completion: @escaping (_ success: Bool) -> Void) {
     let eventStore = EKEventStore()
     eventStore.requestAccess(to: .event) { (granted, error) in
         if (granted) && (error == nil) {
-            do {
-                try eventStore.remove(event, span: .thisEvent)
-                completion(true)
-            } catch let error as NSError {
-                print("failed to remove event with error : \(error)")
-                completion(false)
+            let predicate = eventStore.predicateForEvents(withStart: Date(), end: Date().addingTimeInterval(60 * 60 * 24 * 365), calendars: nil)
+            let events = eventStore.events(matching: predicate)
+            for event in events {
+                if event.title == title {
+                    do {
+                        try eventStore.remove(event, span: .thisEvent)
+                        completion(true)
+                    } catch let error as NSError {
+                        print("failed to remove event with error : \(error)")
+                        completion(false)
+                    }
+                }
             }
-        } else {
-            completion(false)
         }
     }
 }
 
-//MARK: - Update event in calendar
-public func updateEventInCalendar(event: EKEvent, title: String, description: String, startDate: Date, endDate: Date, completion: @escaping (_ success: Bool) -> Void) {
+//MARK: - Update event in calendar by title
+func updateEventInCalendar(title: String, description: String, startDate: Date, endDate: Date, completion: @escaping (_ success: Bool) -> Void) {
     let eventStore = EKEventStore()
     eventStore.requestAccess(to: .event) { (granted, error) in
         if (granted) && (error == nil) {
-            event.title = title
-            event.startDate = startDate
-            event.endDate = endDate
-            event.notes = description
-            do {
-                try eventStore.save(event, span: .thisEvent)
-                completion(true)
-            } catch let error as NSError {
-                print("failed to save event with error : \(error)")
-                completion(false)
+            let predicate = eventStore.predicateForEvents(withStart: Date(), end: Date().addingTimeInterval(60 * 60 * 24 * 365), calendars: nil)
+            let events = eventStore.events(matching: predicate)
+            for event in events {
+                if event.title == title {
+                    event.title = title
+                    event.startDate = startDate
+                    event.endDate = endDate
+                    event.notes = description
+                    do {
+                        try eventStore.save(event, span: .thisEvent)
+                        completion(true)
+                    } catch let error as NSError {
+                        print("failed to save event with error : \(error)")
+                        completion(false)
+                    }
+                }
             }
-        } else {
-            completion(false)
         }
     }
 }
-
